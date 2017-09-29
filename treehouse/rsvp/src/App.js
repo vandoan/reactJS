f2b965d8ce8b7d4b53c15ce30de1dd4824ca97cd
vandoan <vandoona@gmail.com>
2017-09-29 14:24:41 -0700

    treehouse work

---
+++ treehouse/rsvp/src/App.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import './App.css';
+import GuestList from './GuestList';
+
+class App extends Component {
+
+  state = {
+    guests: [
+      {
+        name: "Treasure",
+        isConfirmed: false,
+        isEditing: false
+      },
+      {
+        name: "Nick",
+        isConfirmed: true,
+        isEditing: false
+      },
+      {
+        name: "Matt K.",
+        isConfirmed: true,
+        isEditing: true
+      }
+    ]
+  }
+
+  // getConfirmedGuests
+  // nonConfirmed guests
+
+  toggleGuestPropertyAt = (property, indexToChange) =>
+    this.setState({
+      guests: this.state.guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+              ...guest, // transfers all keys and values
+              [property]: !guest[property]
+          };
+        }
+        return guest;
+      })
+    });
+  
+  toggleConfirmationAt = index => 
+    this.toggleGuestPropertyAt("isConfirmed", index);
+  
+  toggleEditingAt = index => 
+    this.toggleGuestPropertyAt("isEditing", index); 
+
+  getTotaleInvited = () => this.state.guests.length;
+
+  render() {
+    return (
+      <div className="App">
+      <header>
+        <h1>RSVP</h1>
+        <p>A Treehouse App</p>
+        <form>
+            <input type="text" value="Safia" placeholder="Invite Someone" />
+            <button type="submit" name="submit" value="submit">Submit</button>
+        </form>
+      </header>
+      <div className="main">
+        <div>
+          <h2>Invitees</h2>
+          <label>
+            <input type="checkbox" /> Hide those who haven't responded
+          </label>
+        </div>
+        <table className="counter">
+          <tbody>
+            <tr>
+              <td>Attending:</td>
+              <td>2</td>
+            </tr>
+            <tr>
+              <td>Unconfirmed:</td>
+              <td>1</td>
+            </tr>
+            <tr>
+              <td>Total:</td>
+              <td>3</td>
+            </tr>
+          </tbody>
+        </table>
+        
+        <GuestList
+          guests={this.state.guests}
+          toggleConfirmationAt={this.toggleConfirmationAt}
+          toggleEditingAt={this.toggleEditingAt}
+          />
+
+      </div>
+    </div>
+    );
+  }
+}
+
+export default App;
---
+++ treehouse/rsvp/src/GuestList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Guest from './Guest';
+
+
+const GuestList = props => 
+
+	<ul>
+    {props.guests.map((guest, index) => 
+      <Guest
+        key={index}
+        name={guest.name} 
+        handleConfirmation={() => props.toggleConfirmationAt(index)}
+        handleToggleEditing={() => props.toggleEditingAt(index)}
+        isEditing={guest.isEditing}
+        isConfirmed={guest.isConfirmed} />
+ 	  )}
+  </ul>;
+
+GuestList.propTypes = {
+	guests: PropTypes.array.isRequired,
+  toggleConfirmationAt: PropTypes.func.isRequired,
+  toggleEditingAt: PropTypes.func.isRequired
+}
+export default GuestList;
